40189767b2121b10923796a0be3959df776235c2
Arjun Gupta <145846275+Arjun0335@users.noreply.github.com>
2025-02-21 12:58:19 +0530

    Update love.py

--- love.py
+++ love.py
@@ -62,7 +62,7 @@ Tum meri duniya ho, aur tumhare bina sab adhoora lagta hai. 🌟
 st.markdown(f'<div class="apology-text">{apology_text}</div>', unsafe_allow_html=True)
 
 # Display an online image of flowers
-flower_image_url = "https://www.publicdomainpictures.net/pictures/320000/velka/beautiful-flower-bouquet.jpg"
+flower_image_url = "https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwi234brntSLAxWCJEQIHWQ8Al0YABAjGgJkeg&ae=2&aspm=1&co=1&ase=5&gclid=Cj0KCQiAwtu9BhC8ARIsAI9JHakoRxme4oSvdp3ZOb88rhpc8L45WcBbE_FEyxRFYmJtxes2ZjJPe28aAnNLEALw_wcB&ei=8yq4Z-r2EJSt4-EPkpCy0QM&ohost=www.google.com&cid=CAESVeD2CHeEqIZKTUOrIP11A94Rr-JOTvI2eHzSY5J3jbgypM1Puylx-JtNT8WniLFzMiTM_SITRXfSHiJh8rbrvtXqOr7ppQPlNikDHdQNc09dzDTXXdg&sig=AOD64_0x-vKuLDxX49OGbiQrhmU-FZF9AQ&ctype=5&q=&sqi=2&ved=2ahUKEwiqvPrqntSLAxWU1jgGHRKILDoQwg8oAXoECAgQDA&adurl="
 st.image(flower_image_url, caption="Sending virtual flowers 🌸")
 
 # Button to reveal a special message
